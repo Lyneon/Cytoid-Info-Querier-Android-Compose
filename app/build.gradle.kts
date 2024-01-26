916039ae4ff216418c65b854f572872040b8df33
Lyneon <lyneon@outlook.com>
2024-01-26 14:06:15 +0800

    更新部分依赖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,7 @@ android {
 }
 
 dependencies {
-    val navVersion = "2.7.5"
+    val navVersion = "2.7.6"
     val appCenterSdkVersion = "5.0.3"
 
     implementation("com.microsoft.appcenter:appcenter-analytics:${appCenterSdkVersion}")
@@ -71,8 +71,8 @@ dependencies {
     implementation("dev.shreyaspatil:capturable:1.0.3")
 
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
