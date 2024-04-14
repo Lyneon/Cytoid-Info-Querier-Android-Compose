cdc39b96dfaf2f4bab52daf8fea71cb7af437a02
Lyneon <69835744+Lyneon@users.noreply.github.com>
2024-04-14 12:55:44 +0800

    1.5.0：异常收集服务由AppCenter更改为Sentry

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.lyneon.cytoidinfoquerier"
         minSdk = 24
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.4.2"
+        versionCode = 9
+        versionName = "1.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
