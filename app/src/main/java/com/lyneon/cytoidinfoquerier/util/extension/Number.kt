23a92af325179dce44508acbfb193665b6b7ae10
Lyneon <69835744+Lyneon@users.noreply.github.com>
2024-05-31 09:49:50 +0800

    为Number.kt编写了Java Doc

--- app/src/main/java/com/lyneon/cytoidinfoquerier/util/extension/Number.kt
+++ app/src/main/java/com/lyneon/cytoidinfoquerier/util/extension/Number.kt
@@ -3,6 +3,12 @@ package com.lyneon.cytoidinfoquerier.util.extension
 import java.math.RoundingMode
 import java.text.DecimalFormat
 
+/**
+ * 将数值按指定的保留的小数位数格式化为字符串
+ *
+ * @param digits 小数点后的保留位数
+ * @param roundingMode 使用的近似模式
+ */
 fun <T : Number> T.setPrecision(
     digits: Int,
     roundingMode: RoundingMode = RoundingMode.CEILING
