40c88f96fa0776de5db4de2e5a4869497935fd13
Lyneon <69835744+Lyneon@users.noreply.github.com>
2024-07-15 21:57:02 +0800

    更新依赖与AGP插件

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,7 @@ android {
 dependencies {
     val navVersion = "2.7.7"
     val media3Version = "1.3.1"
-    val composeBomVersion = "2024.05.00"
+    val composeBomVersion = "2024.06.00"
 
     implementation("io.sentry:sentry-android:7.8.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
@@ -69,14 +69,15 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.6.0")
     implementation("com.tencent:mmkv:1.3.3")
     implementation("androidx.navigation:navigation-compose:$navVersion")
-    implementation("androidx.compose.material:material-icons-extended:1.6.7")
+    implementation("androidx.compose.material:material-icons-extended:1.6.8")
     implementation("dev.shreyaspatil:capturable:1.0.3")
     implementation("com.patrykandpatrick.vico:compose-m3:1.14.0")
     implementation("androidx.media3:media3-exoplayer:$media3Version")
+    implementation("androidx.compose.material3:material3-window-size-class:1.2.1")
 
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.3")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation(platform("androidx.compose:compose-bom:$composeBomVersion"))
     implementation("androidx.compose.ui:ui")
@@ -85,8 +86,8 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation(platform("androidx.compose:compose-bom:$composeBomVersion"))
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:$composeBomVersion"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
