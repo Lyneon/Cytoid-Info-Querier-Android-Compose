40716ba7f05b226d2ead47511262203fa024e146
Lyneon <69835744+Lyneon@users.noreply.github.com>
2024-08-20 23:06:39 +0800

    chore: 更新版本号至2.1.0(11)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.lyneon.cytoidinfoquerier"
         minSdk = 24
         targetSdk = 34
-        versionCode = 10
-        versionName = "2.0.0"
+        versionCode = 11
+        versionName = "2.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
--- app/src/main/java/com/lyneon/cytoidinfoquerier/ui/compose/screen/LevelScreen.kt
+++ app/src/main/java/com/lyneon/cytoidinfoquerier/ui/compose/screen/LevelScreen.kt
@@ -1,15 +1,22 @@
 package com.lyneon.cytoidinfoquerier.ui.compose.screen
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.lyneon.cytoidinfoquerier.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -24,8 +31,17 @@ fun LevelScreen() {
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues)
+                .padding(horizontal = 16.dp)
         ) {
-
+            Card {
+                Row(
+                    modifier = Modifier.padding(16.dp),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    Icon(imageVector = Icons.Default.Info, contentDescription = null)
+                    Text(text = "还没做完！先贵阳一会")
+                }
+            }
         }
     }
 }
