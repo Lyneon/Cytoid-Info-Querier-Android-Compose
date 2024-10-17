a33a9f5bc9af0d849a9a094b15f6e950e510b888
Lyneon <69835744+Lyneon@users.noreply.github.com>
2024-10-17 22:29:06 +0800

    chore: 更新版本至2.2.1(14)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.lyneon.cytoidinfoquerier"
         minSdk = 24
         targetSdk = 34
-        versionCode = 13
-        versionName = "2.2.0"
+        versionCode = 14
+        versionName = "2.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
