aeb28f9c0a973dd15fd1d486a5137d6d84fbe1cc
Lyneon <69835744+Lyneon@users.noreply.github.com>
2024-11-03 21:03:22 +0800

    chore: 更新依赖项

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,27 +59,27 @@ android {
 }
 
 dependencies {
-    val composeBomVersion = "2024.09.02"
+    val composeBomVersion = "2024.10.01"
 
     implementation("io.sentry:sentry-android:7.12.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
     implementation("io.coil-kt:coil-compose:2.7.0")
     implementation("com.tencent:mmkv:1.3.7")
-    implementation("androidx.navigation:navigation-compose:2.8.1")
-    implementation("androidx.compose.material:material-icons-extended:1.7.2")
+    implementation("androidx.navigation:navigation-compose:2.8.3")
+    implementation("androidx.compose.material:material-icons-extended:1.7.5")
     implementation("dev.shreyaspatil:capturable:2.1.0")
     implementation("com.patrykandpatrick.vico:compose:2.0.0-alpha.28")
     implementation("com.patrykandpatrick.vico:compose-m3:2.0.0-alpha.28")
     implementation("androidx.media3:media3-exoplayer:1.4.1")
-    implementation("androidx.compose.material3:material3-window-size-class:1.3.0")
+    implementation("androidx.compose.material3:material3-window-size-class:1.3.1")
     implementation("com.google.androidbrowserhelper:androidbrowserhelper:2.5.0")
 
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.6")
-    implementation("androidx.activity:activity-compose:1.9.2")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
+    implementation("androidx.activity:activity-compose:1.9.3")
     implementation(platform("androidx.compose:compose-bom:$composeBomVersion"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
