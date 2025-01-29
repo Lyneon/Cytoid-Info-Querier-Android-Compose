d2b7c75bcfdaf8ec38c55480d4f01b3183f56f20
Lyneon <69835744+Lyneon@users.noreply.github.com>
2025-01-29 17:55:33 +0800

    feat: 关卡详情页增加评分显示

--- app/src/main/java/com/lyneon/cytoidinfoquerier/util/CytoidLevelUtils.kt
+++ app/src/main/java/com/lyneon/cytoidinfoquerier/util/CytoidLevelUtils.kt
@@ -2,6 +2,8 @@ package com.lyneon.cytoidinfoquerier.util
 
 import com.lyneon.cytoidinfoquerier.data.constant.CytoidConstant
 import com.lyneon.cytoidinfoquerier.data.constant.OkHttpSingleton
+import com.lyneon.cytoidinfoquerier.data.model.webapi.LevelRating
+import com.lyneon.cytoidinfoquerier.json
 import okhttp3.Request
 
 object CytoidLevelUtils {
@@ -12,6 +14,22 @@ object CytoidLevelUtils {
             .addHeader("User-Agent", CytoidConstant.clientUA)
             .build()
         val response = OkHttpSingleton.instance.newCall(request).execute()
-        return response.headers.get("X-Total-Entries")?.toInt() ?: -1
+        return response.headers["X-Total-Entries"]?.toInt() ?: -1
+    }
+
+    fun getLevelRating(levelUid: String): LevelRating {
+        val request = Request.Builder()
+            .url("https://services.cytoid.io/levels/$levelUid/ratings")
+            .removeHeader("User-Agent")
+            .addHeader("User-Agent", CytoidConstant.clientUA)
+            .build()
+        val response = OkHttpSingleton.instance.newCall(request).execute()
+        if (response.isSuccessful) {
+            return json.decodeFromString(
+                response.body?.string() ?: throw Exception("Failed to get level rating")
+            )
+        } else {
+            throw Exception("Failed to get level rating")
+        }
     }
 }
