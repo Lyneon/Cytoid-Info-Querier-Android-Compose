9ec6cb5d59149b6ec83f2475c389f6f858b41590
Lyneon <69835744+Lyneon@users.noreply.github.com>
2025-02-23 20:34:22 +0800

    feat: Profile页面添加经验相关展示

--- app/src/main/java/com/lyneon/cytoidinfoquerier/ui/compose/screen/ProfileScreen.kt
+++ app/src/main/java/com/lyneon/cytoidinfoquerier/ui/compose/screen/ProfileScreen.kt
@@ -24,7 +24,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
