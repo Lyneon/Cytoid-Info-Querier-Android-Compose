e79ed08d336476707ef168e2ec744dfe4c1189a8
Lyneon <69835744+Lyneon@users.noreply.github.com>
2025-04-10 18:00:40 +0800

    feat(i18n): 补全导入关卡页面的英文翻译；新增了本地缓存加载提示信息

--- app/src/main/java/com/lyneon/cytoidinfoquerier/data/repository/RecentRecordsRepository.kt
+++ app/src/main/java/com/lyneon/cytoidinfoquerier/data/repository/RecentRecordsRepository.kt
@@ -1,10 +1,13 @@
 package com.lyneon.cytoidinfoquerier.data.repository
 
+import com.lyneon.cytoidinfoquerier.BaseApplication
+import com.lyneon.cytoidinfoquerier.R
 import com.lyneon.cytoidinfoquerier.data.constant.RecordQueryOrder
 import com.lyneon.cytoidinfoquerier.data.constant.RecordQuerySort
 import com.lyneon.cytoidinfoquerier.data.datasource.LocalDataSource
 import com.lyneon.cytoidinfoquerier.data.datasource.RemoteDataSource
 import com.lyneon.cytoidinfoquerier.data.model.graphql.RecentRecords
+import com.lyneon.cytoidinfoquerier.util.extension.showToast
 import com.lyneon.cytoidinfoquerier.util.getLastRecentRecordsCacheTime
 
 class RecentRecordsRepository {
@@ -20,9 +23,14 @@ class RecentRecordsRepository {
         }
     else {
         val lastRecentRecordsCacheTime = cytoidID.getLastRecentRecordsCacheTime()
-        if (System.currentTimeMillis() - lastRecentRecordsCacheTime <= 1000 * 60 * 60 * 6)
+        val passedTime = System.currentTimeMillis() - lastRecentRecordsCacheTime
+        if (passedTime <= 1000 * 60 * 60 * 6) {
+            BaseApplication.context.getString(
+                R.string.loading_local_cache,
+                (passedTime / 1000 / 60).toString()
+            ).showToast()
             LocalDataSource.loadRecentRecords(cytoidID, lastRecentRecordsCacheTime)
-        else
+        } else
             RemoteDataSource.fetchRecentRecords(cytoidID, count, sort, order).also {
                 if (it.data.profile != null) LocalDataSource.saveRecentRecords(cytoidID, it)
             }
