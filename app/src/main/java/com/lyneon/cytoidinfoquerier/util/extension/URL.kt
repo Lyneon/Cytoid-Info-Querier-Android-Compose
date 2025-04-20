b457e4d4916e6262a15a09c7ef131de3dd14a11b
Lyneon <69835744+Lyneon@users.noreply.github.com>
2025-04-20 12:16:30 +0800

    fix: 修复生成图片时不会绘制曲绘的问题
    - 修复了URL.toBitmap()在解码图片完成前就关闭流的错误

--- app/src/main/java/com/lyneon/cytoidinfoquerier/util/extension/URL.kt
+++ app/src/main/java/com/lyneon/cytoidinfoquerier/util/extension/URL.kt
@@ -1,23 +1,25 @@
 package com.lyneon.cytoidinfoquerier.util.extension
 
 import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import androidx.core.net.toUri
 import com.lyneon.cytoidinfoquerier.BaseApplication
 import com.lyneon.cytoidinfoquerier.data.constant.okHttpClient
 import okhttp3.Request
+import java.io.IOException
 import java.net.URL
 
-fun URL.toBitmap() = BitmapFactory.decodeStream(
-    okHttpClient.newCall(
-        Request.Builder()
-            .url(this)
-            .removeHeader("User-Agent").addHeader("User-Agent", "CytoidClient/2.1.1")
-            .build()
-    ).execute().use {
-        it.body?.byteStream()
-    }
-) ?: throw Exception()
+fun URL.toBitmap(): Bitmap = okHttpClient.newCall(
+    Request.Builder()
+        .url(this)
+        .removeHeader("User-Agent").addHeader("User-Agent", "CytoidClient/2.1.1")
+        .build()
+).execute().use { response ->
+    val body = response.body ?: throw IOException("Response body is null")
+    val inputStream = body.byteStream()
+    BitmapFactory.decodeStream(inputStream) ?: throw IOException("Failed to decode bitmap stream")
+}
 
 fun URL.openInBrowser() {
     val intent = Intent(Intent.ACTION_VIEW).apply {
